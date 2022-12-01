f2ea3133ff811531a7a61be782c3cec80e062a41
Sofia Gromovaya <sonya.gromovaya2004@gmail.com>
2022-12-01 06:46:45 +0200

    Updates

---
+++ static/js/666.2ae10275.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[666],{7666:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var s="PageNotFound_container__unC-t",a="PageNotFound_messege__-J1JQ",c=t(184),i=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("div",{className:s,children:(0,c.jsx)("div",{className:a,children:"Page not found!"})})})}}}]);
+//# sourceMappingURL=666.2ae10275.chunk.js.map
